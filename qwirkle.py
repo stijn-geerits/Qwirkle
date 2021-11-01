e9edb87cfcf6985af4841d88c3256fa580bc63d0
lael-jonckers <lael.jonckers@student.kuleuven.be>
2021-11-01 14:51:35 +0100

    Changed background, altered title graphic and added it to the main menu

--- qwirkle.py
+++ qwirkle.py
@@ -3,15 +3,25 @@
 import pygame
 
 if __name__ == "__main__":
-	#window setup
-	window = pygame.display.set_mode([800, 600])
-	pygame.display.set_caption("Qwirkle")
-	
 	#set constants
 	FPS = 60
+	WINSIZE = [800, 600]
 	#set variables
 	clock = pygame.time.Clock()
 	loop = True
+	
+	#window setup
+	window = pygame.display.set_mode(WINSIZE)
+	pygame.display.set_caption("Qwirkle")
+	#change window background
+	window.fill((200, 200, 200))
+	#show qwirkle title graphic
+	qwirkle = pygame.image.load("Graphics/qwirkle.png")
+	qwirkleRect = qwirkle.get_rect()
+	qwirkleRect.midtop = [int(WINSIZE[0] * .5), int(WINSIZE[1] * .05)]
+	window.blit(qwirkle, qwirkleRect);
+	#update the display
+	pygame.display.update()
 	
 	### main game loop ###
 	while loop == True:

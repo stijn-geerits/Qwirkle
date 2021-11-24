534855c9d235059e15b78fd1bb706aa6ee7c0624
VVisp <44241775+VVisp@users.noreply.github.com>
2021-11-24 20:55:39 +0100

    Added first step of gameplay to testgame

--- testgame.py
+++ testgame.py
@@ -5,6 +5,7 @@ from tile import Tile
 
 
 def main():
+    # Init the game
     mybag = Bag()
 
     speler1 = Player(1, "Stijn")
@@ -16,16 +17,22 @@ def main():
     for row in board:
         print(row)
 
+    # Kies eerste speler
     mygame.player_on_hand = mygame.players[0]
-    print(f"{mygame.get_player_on_hand().get_name()} is aan de beurt")
+    current_player = mygame.get_player_on_hand()
+    print(f"{current_player.get_name()} is aan de beurt")
 
-    tiles = mybag.take_tiles(6)
-    speler1.add_to_hand(tiles)
-
-    tiles_info = make_tiles_printable(speler1.get_hand())
-
+    # Toon blokken in hand van speler
+    current_hand = current_player.get_hand()
+    tiles_info = make_tiles_printable(current_hand)
     print(tiles_info)
-    play_tiles, play_positions = handle_player_input(speler1)
+
+    play_tile_ids, play_positions = handle_player_input()
+    play_tiles = []
+    for tile in current_hand:
+        if tile.get_id() in play_tile_ids:
+            play_tiles.append(tile)
+
     play_tiles_p = make_tiles_printable(play_tiles)
     print(f"Je hebt gespeeld: {play_tiles_p}, {play_positions}")
 
@@ -42,25 +49,19 @@ def make_tiles_printable(tiles):
     return printable_tiles
 
 
-def handle_player_input(speler):
-    tile_index_str = ''
-    tiles = []
-    while tile_index_str != 'x':
-        tile_index_str = input("Geef ID van tile: ")
-        if tile_index_str.isdigit():
-            tiles.append(speler.take_from_hand(int(tile_index_str)))
+def handle_player_input():
+    print("Geef de id's van de blokjes en hun locaties die je wil plaatsen." + "\n" + "bv: 15;(3,5);25;(6,9)")
+    user_input = str(input(':'))
+    user_input = user_input.split(";")
 
-    position_str_y = ''
-    position_str_x = ''
-    positions = []
-    while position_str_y != 'x' or position_str_x != 'x':
-        position_str_y = input("Geef positie in (y): ")
-        position_str_x = input("Geef positie in (x): ")
-        if position_str_y.isdigit():
-            if position_str_x.isdigit():
-                positions.append((position_str_y, position_str_x))
+    tile_ids = list(map(int, user_input[::2]))
 
-    return tiles, positions
+    position_strings = user_input[1::2]
+    tmp1 = [el.strip('()') for el in position_strings]
+    tmp2 = [el.split(',') for el in tmp1]
+    positions = [tuple(int(el2) for el2 in el) for el in tmp2]
+
+    return tile_ids, positions
 
 
 if __name__ == "__main__":
